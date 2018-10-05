78552a7e236217710e42c95610705da2f94d8adb
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2018-10-05 16:13:13 -0300

    Add create method to ReviewSerializer

--- src/core/serializers.py
+++ src/core/serializers.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'first_name', 'last_name', 'email')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email')
 
 
 class CompanySerializer(serializers.ModelSerializer):
@@ -21,8 +21,25 @@ class CompanySerializer(serializers.ModelSerializer):
 class ReviewSerializer(serializers.ModelSerializer):
     reviewer = UserSerializer(read_only=True)
     company = CompanySerializer()
+    ip = serializers.IPAddressField(protocol='both', read_only=True)
+    submission_date = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = models.Review
         fields = ('id', 'rating', 'title', 'summary', 'ip', 'company',
                   'reviewer', 'submission_date')
+
+    def create(self, validated_data):
+        request = self.context['request']
+        client_ip = request.META.get('REMOTE_ADDR')
+        validated_data['ip'] = client_ip
+
+        validated_data['reviewer'] = request.user
+
+        company_data = validated_data.pop('company')
+        company = models.Company.objects.get_or_create(
+            name=company_data.get('name')
+        )[0]
+        validated_data['company'] = company
+
+        return super().create(validated_data)
